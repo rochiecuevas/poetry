4d16d0ac166541d795cb193acc157078470265ca
Rochie Cuevas <rochiecuevas@gmail.com>
2019-05-12 00:57:07 -0700

    updating javascript for the poets selection

--- static/js/app.js
+++ static/js/app.js
@@ -1,50 +1,126 @@
-// Set the urls
+// Set the urls and default values
 var urlMetadata = "/metadata"
 var urlPoem = "/data"
+var defaultPoet = "Robert Frost";
+var defaultPoem = "October";
 
-// Use the metadata
+// Define variables to be used in the functions
+var poemTitle;
+var poetName;
+var selection;
+
 d3.json(urlMetadata).then(function(trace){
     var data = [trace][0];
     console.log(data);
 
-    // When a selection has been made
-    // (1) Create a variable for poem title
-    var poemTitle = d3.select("#poem");
+    // Create a variable for poets
+    poetName = d3.select("#poet");
 
-    // (2) Create a list of options
-    var optionsList = [];
-    for (i = 0; i < data.length; i ++){
-        optionsList[i] = data[i]["Title"];
+    // Create a list of poet options
+    var poetOptions = [];
+    var poets = [];
+    for (var i = 0; i < data.length; i ++){
+        poets[i] = data[i]["Poet"];
+        poetOptions = new Set(poets);
+        poetOptions = Array.from(poetOptions);
     };
-    console.log(optionsList);
+    console.log(poetOptions);
 
-    // (3) Populate the select field with poem titles
-    var options = poemTitle
+    // Populate the poet options
+    var options1 = poetName
         .selectAll("#title")
-        .data(optionsList).enter()
+        .data(poetOptions).enter()
         .append("option")
         .text(function(title){
             return title;
         })
         .attr("title", "options");
-       
+    
+    // Use the default poet
+    options1.property("selected", function(d){
+        return d === defaultPoet
+        });
 
-    // (4) Default selection = "October"
-    var defaultx = "October";
-    options.property("selected", function(d){
-        return d === defaultx
-    }); 
+    // Use the default poet to create a subset of poem titles for the title options list
+    var urlMetadata1 = `/metadata/${defaultPoet}`;
+    console.log(urlMetadata1);
+    extractTitles(urlMetadata1);
 
-    // (5) Fill in the table based on the default selected poem
-    var urlMetadata2 = `/metadata/${defaultx}`;
+    // Extract metadata for the (default) selected poem of the selected poet
+    var urlMetadata2 = `/metadata/${defaultPoet}/${defaultPoem}`;
     console.log(urlMetadata2);
+    extractMeta(defaultPoem, urlMetadata2);
 
-    var urlPoem2 = `/data/${defaultx}`;
-    console.log(urlPoem2)
-    document.getElementById("title").innerHTML = defaultx;
+    // Extract metadata for the (default) selected poem of the selected poet
+    var urlMetadata2 = `/metadata/${defaultPoet}/${defaultPoem}`;
+    console.log(urlMetadata2);
+    extractMeta(defaultPoem, urlMetadata2);
 
-    // DEFAULT: Use the default url to fill the table with metadata
-    d3.json(urlMetadata2).then(function(trace){
+    // Create a plot of keywords for the default poem
+    var urlPoem = `/data/${defaultPoem}`;
+    console.log(urlPoem);
+    extractPoem(urlPoem);
+
+    //  Change the poet
+    function handleChangePoet(){
+        selection = poetName.property("value");
+        console.log(selection);
+
+        var urlMetadata1 = `/metadata/${selection}`;
+        console.log(urlMetadata1);
+        extractTitles(urlMetadata1);
+
+        return selection;
+    };
+    poetName.on("change", handleChangePoet);
+});
+
+// Functions defined
+function extractTitles(url){
+    d3.json(url).then(function(trace){
+        var data = [trace][0];
+        console.log(data);
+
+        // When a selection has been made
+        var poemTitle = d3.select("#poem");
+        var optionsList = [];
+        for (i = 0; i < data["title"].length; i ++){
+            optionsList[i] = data["title"][i];
+        };
+        console.log(optionsList);
+
+        // Populate the select field with poem titles
+        var options = poemTitle
+            .selectAll("#title")
+            .data(optionsList).enter()
+            .append("option")
+            .text(function(title){
+                return title;
+            })
+            .attr("title", "options");
+
+        // Use the default poet
+        options.property("selected", function(d){
+            return d === optionsList[0]
+            });  
+            
+        // Get the metadata for the table
+        var selection = poetName.property("value");
+        var selection1 = poemTitle.property("value");
+        var url = `/metadata/${selection}/${selection1}`;
+
+        extractMeta(selection1, url);
+        return selection1;
+    });     
+};
+
+function extractMeta(poem, url){
+    // Fill the metadata table with information of the default poem
+    console.log(poem);
+    console.log(url);
+    document.getElementById("title").innerHTML = poem;
+
+    d3.json(url).then(function(trace){
         var data = [trace][0];
         console.log(data);
 
@@ -61,89 +137,97 @@ d3.json(urlMetadata).then(function(trace){
         for (var i = 0; i < tdList.length; i ++){
             tdList[i].text(data[catList[i]])
         };
-    });
 
-    // DEFAULT: Use the default url to print the poem
-    d3.json(urlPoem2).then(function(trace){
-        var data = [trace][0]["lines"];
-        var res = data.split(/\n/).join("<br>")
+        var poemTitle = d3.select("#poem");
+        var selection1 = poemTitle.property("value");
+        var url2 = urlPoem + `/${selection1}`;
+        extractPoem(url2);
+    });
+};
+
+function extractPoem(url){
+    console.log(url);
+    d3.json(url).then(function(trace){
+        var data = [trace][0];
+        var res = data["lines"];
         console.log(res);
         document.getElementById("lines").innerHTML = res;
 
-        // DEFAULT: Create a bar chart using TF-IDF
-            var data2 = [trace][0];
-            console.log(data2);
-            data2["type"] = "bar";
-            data2["x"] = data2["word"];
-            data2["y"] = data2["TF-IDF"];
+        // Construct the TF-IDF bar plot
+        data["type"] = "bar";
+        data["x"] = data["word"];
+        data["y"] = data["TF-IDF"];
 
-            var layout = {
-                title: "TF-IDF of the five most important words",
-                showlegend: false,
-                xaxis: {title: "Word"},
-                yaxis: {title: "Word importance (TF-IDF)"}
-            };
+        var layout = {
+            title: "TF-IDF of the five most important words",
+            showlegend: false,
+            xaxis: {title: "Word"},
+            yaxis: {title: "Word importance (TF-IDF)"}
+        };
 
-            Plotly.newPlot("bar", [data2], layout);
-        });
+        Plotly.newPlot("bar", [data], layout);
+    });
+};
 
 
-    // (5.1) Select a sample
-    function handleChange(){
-        var selection = poemTitle.property("value");
-        console.log(selection);
 
-        // (5.2) Change the metadata url to include the selection
-        var urlMetadata1 = `/metadata/${selection}`;
-        console.log(urlMetadata1);
 
-        // (5.3) Use the new url to update the table with metadata
-        d3.json(urlMetadata1).then(function(trace){
-            var data = [trace][0];
-            console.log(data);
+// //     // (5.1) Select a sample
+// //     function handleChange(){
+// //         var selection = poemTitle.property("value");
+// //         console.log(selection);
 
-            // (5.3.1) Get the values that match the keys of each item in the array
-            var tdLength = d3.select("#poem_length");
-            var tdYear = d3.select("#publication_year");
-            var tdSentiment = d3.select("#sentiment");
-            var tdLexDiv = d3.select("#lex_div")
+// //         // (5.2) Change the metadata url to include the selection
+// //         var urlMetadata1 = `/metadata/${selection}`;
+// //         console.log(urlMetadata1);
 
-            var tdList = [tdLength, tdYear, tdSentiment, tdLexDiv];
-            var catList = ["poem_length", "publication_year", "sentiment", "lexical_diversity"];
+// //         // (5.3) Use the new url to update the table with metadata
+// //         d3.json(urlMetadata1).then(function(trace){
+// //             var data = [trace][0];
+// //             console.log(data);
 
-            // (5.3.2) Populate the HTML table with the metadata for each poem
-            for (var i = 0; i < tdList.length; i ++){
-                tdList[i].text(data[catList[i]])
-            };
-        });
-        // (5.4) Change the data url to include the selection
-        var urlPoem1 = `/data/${selection}`;
-        console.log(urlPoem1);
-        document.getElementById("title").innerHTML = selection;
+// //             // (5.3.1) Get the values that match the keys of each item in the array
+// //             var tdLength = d3.select("#poem_length");
+// //             var tdYear = d3.select("#publication_year");
+// //             var tdSentiment = d3.select("#sentiment");
+// //             var tdLexDiv = d3.select("#lex_div")
 
-        // (5.5) Use the new url to get the lines of the poem
-        d3.json(urlPoem1).then(function(trace){
-            var data = [trace][0]["lines"];
-            var res = data.split(/\n/).join("<br>")
-            console.log(res);
-            document.getElementById("lines").innerHTML = res;
+// //             var tdList = [tdLength, tdYear, tdSentiment, tdLexDiv];
+// //             var catList = ["poem_length", "publication_year", "sentiment", "lexical_diversity"];
 
-            // (5.6) Create a bar chart using TF-IDF
-            var data2 = [trace][0];
-            console.log(data2);
-            data2["type"] = "bar";
-            data2["x"] = data2["word"];
-            data2["y"] = data2["TF-IDF"];
+// //             // (5.3.2) Populate the HTML table with the metadata for each poem
+// //             for (var i = 0; i < tdList.length; i ++){
+// //                 tdList[i].text(data[catList[i]])
+// //             };
+// //         });
+// //         // (5.4) Change the data url to include the selection
+// //         var urlPoem1 = `/data/${selection}`;
+// //         console.log(urlPoem1);
+// //         document.getElementById("title").innerHTML = selection;
 
-            var layout = {
-                title: "TF-IDF of the five most important words",
-                showlegend: false,
-                xaxis: {title: "Word"},
-                yaxis: {title: "Word importance (TF-IDF)"}
-            };
+// //         // (5.5) Use the new url to get the lines of the poem
+// //         d3.json(urlPoem1).then(function(trace){
+// //             var data = [trace][0]["lines"];
+// //             var res = data.split(/\n/).join("<br>")
+// //             console.log(res);
+// //             document.getElementById("lines").innerHTML = res;
 
-            Plotly.newPlot("bar", [data2], layout);
-        });
-    };
-    poemTitle.on("change", handleChange);
-});
+// //             // (5.6) Create a bar chart using TF-IDF
+// //             var data2 = [trace][0];
+// //             console.log(data2);
+// //             data2["type"] = "bar";
+// //             data2["x"] = data2["word"];
+// //             data2["y"] = data2["TF-IDF"];
+
+// //             var layout = {
+// //                 title: "TF-IDF of the five most important words",
+// //                 showlegend: false,
+// //                 xaxis: {title: "Word"},
+// //                 yaxis: {title: "Word importance (TF-IDF)"}
+// //             };
+
+// //             Plotly.newPlot("bar", [data2], layout);
+// //         });
+// //     };
+// //     poemTitle.on("change", handleChange);
+// // });
